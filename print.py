e26adfecf3d0ffa8bf1e7362005e3e516dd40d54
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2025-12-22 19:10:34 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,14 @@
+# Python Print Practice 
+# This repository contains very basic Python print statement programs.
+
+# This is my starting point for Python and AI/ML learning.
+
+print("Day 1 ->  One Day")
+
+print("Learning Python for AI & ML")
+
+print("Hello World!")
+
+print("Sanskar Chourasiya :)\n" * 15)
+
+print("Consistency > Motivation\n" * 5)
