68b39dbc0f2da8afcdb88ef83a94ac11031d011a
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-01-09 06:17:38 +0530

    added round.py
    Added input prompts for variables a and b, and printed their sum.

--- round.py
+++ round.py
@@ -4,3 +4,7 @@ y = int(input("what's y? " ))#999
 z = round(x+y)
 print(z)#1000
 print(f"{z : ,}") #1,000
+
+a = float(input("what's a? " ))#1.2
+b = float(input("what's b? " ))#3.4
+print(a+b)#4.6 rounded off to 5
